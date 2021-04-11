50ce4765c478c6dc5e3565cdc7fe437e00c94ad0
Iramboana-Thierry <irambonat0@gmail.com>
2021-04-11 12:07:29 +0200

    improved signin card's appearance in devices with smaller heights

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,78 +1,78 @@
-import { Link, useHistory } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useDispatch, useSelector } from 'react-redux';
-import { loginSchema } from 'utils/AuthValidation';
+import { Link, useHistory } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginSchema } from 'utils/AuthValidation'
 
-import BaseInput from 'components/Shared/BaseInput';
-import BaseButton from 'components/Shared/BaseButton';
-import { authError, logIn } from 'store/actions/authActions';
-import { selectCurrentUser } from 'store/selector/authSelector';
-import { useEffect } from 'react';
-import { appStatus } from 'store/selector/appSelector';
+import BaseInput from 'components/Shared/BaseInput'
+import BaseButton from 'components/Shared/BaseButton'
+import { authError, logIn } from 'store/actions/authActions'
+import { selectCurrentUser } from 'store/selector/authSelector'
+import { useEffect } from 'react'
+import { appStatus } from 'store/selector/appSelector'
 
 const Signin = () => {
-  const dispatch = useDispatch();
-  const { push } = useHistory();
-  const currentUser = useSelector((state) => selectCurrentUser(state));
-  const { isLoading, error } = useSelector((state) => appStatus(state));
+  const dispatch = useDispatch()
+  const { push } = useHistory()
+  const currentUser = useSelector((state) => selectCurrentUser(state))
+  const { isLoading, error } = useSelector((state) => appStatus(state))
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(loginSchema),
-    mode: 'onBlur',
-  });
+    mode: 'onBlur'
+  })
 
   useEffect(() => {
     if (currentUser) {
-      push('/');
+      push('/')
     }
-  }, [currentUser, push]);
+  }, [currentUser, push])
 
   const onSubmit = (data) => {
-    dispatch(logIn(data));
-  };
+    dispatch(logIn(data))
+  }
 
   return (
-    <div className='min-h-screen'>
-      <div className='flex justify-center h-screen/2'>
-        <div className='rounded-lg shadow-lg p-10'>
-          <div className='flex flex-col mb-6'>
-            <h1 className='font-bold text-xl tracking-wider text-center'>
+    <div className="min-h-screen">
+      <div className="flex justify-center">
+        <div className="rounded-lg shadow-lg p-10">
+          <div className="flex flex-col mb-6">
+            <h1 className="font-bold text-xl tracking-wider text-center">
               SIGNIN
             </h1>
-            {error && <p className='text-red-500 text-center'>{error}</p>}
+            {error && <p className="text-red-500 text-center">{error}</p>}
           </div>
           <form onSubmit={handleSubmit(onSubmit)}>
             <BaseInput
               errors={errors.email?.message || error}
-              name='email'
-              placeholder='Email'
+              name="email"
+              placeholder="Email"
               register={register}
-              type='text'
+              type="text"
             />
             <BaseInput
               errors={errors.password?.message || error}
-              name='password'
-              placeholder='Password'
+              name="password"
+              placeholder="Password"
               register={register}
-              type='password'
+              type="password"
             />
             <BaseButton
               disabled={isLoading}
-              type='submit'
+              type="submit"
               name={isLoading ? 'Logging in' : 'Log in'}
             />
           </form>
           <Link
             onClick={() => dispatch(authError(null))}
-            to='/signup'
-            className='inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 mt-2'
+            to="/signup"
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 mt-2"
           >
             No Account? Sign up
           </Link>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Signin;
+export default Signin
